406ebb5aa9cafe343b5e4f79fe77015468026324
Aravind Rajeswaran <rajeswaran.aravind@gmail.com>
2018-07-03 14:56:46 -0700

    update readme and add policy visualizer script

---
+++ dapg/utils/visualize_policy.py
@@ -0,0 +1,27 @@
+import mj_envs
+import click 
+import os
+import gym
+import numpy as np
+import pickle
+from mjrl.utils.gym_env import GymEnv
+
+DESC = '''
+Helper script to visualize policy (in mjrl format).\n
+USAGE:\n
+    Visualizes policy on the env\n
+'''
+
+# MAIN =========================================================
+@click.command(help=DESC)
+@click.option('--env_name', type=str, help='environment to load', required= True)
+@click.option('--policy', type=str, help='absolute path of the policy file', required=True)
+@click.option('--mode', type=str, help='exploration or evaluation mode for policy', default='evaluation')
+def main(env_name, policy, mode):
+    e = GymEnv(env_name)
+    pi = pickle.load(open(policy, 'rb'))
+    # render policy
+    e.visualize_policy(pi, num_episodes=100, horizon=e.horizon, mode=mode)
+
+if __name__ == '__main__':
+    main()
